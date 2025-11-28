33c3f5ba68d294676f057773db10e24f364f9542
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-28 01:34:04 -0300

    Criar Estudos/Revisando.js

---
+++ Estudos/Revisando.js
@@ -0,0 +1,28 @@
+// REVISÃO GERAL JS:
+function helloW(){
+  // Output terminal;
+  console.log("Hello World!");
+  console.log("Inicializando o processamento..."); 
+}
+function ternario(){
+  // Variável let p/ valores não constantes;
+  let v1 = 10;
+  let v2 = 20;
+  // Ternário: teste/ocasiao ? true : false;
+  let teste = v1 > v2 ? "sim":"não";
+  // Estrutura condicional:
+  if(teste == 'não'){
+    //Usando template string '$(var)' ao invés de concatenar '+';
+    console.log(`${v1} não é maior que ${v2}!`);   
+  }
+  else{
+    console.log(`${v1} é maior que ${v2}!`);   
+  }
+}
+try {
+  //helloW();
+  //ternario();
+}
+finally {
+  console.log("Processo finalizado...") 
+}
